56efc62c1a68ce7a3a6bdc9fc858e9d99ccbe415
Kavoavokavo <115181288+Kavoavokavo@users.noreply.github.com>
2023-06-05 02:02:41 +0300

    Add files via upload

---
+++ KrestikNolik/KrestikNolik/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace KrestikNolik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox23 = new System.Windows.Forms.PictureBox();
+            this.pictureBox22 = new System.Windows.Forms.PictureBox();
+            this.pictureBox21 = new System.Windows.Forms.PictureBox();
+            this.pictureBox33 = new System.Windows.Forms.PictureBox();
+            this.pictureBox32 = new System.Windows.Forms.PictureBox();
+            this.pictureBox31 = new System.Windows.Forms.PictureBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nolbox = new System.Windows.Forms.TextBox();
+            this.krestbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox23)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox33)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox32)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox31)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackColor = System.Drawing.Color.DarkOrange;
+            this.pictureBox11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox11.Location = new System.Drawing.Point(137, 114);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(106, 96);
+            this.pictureBox11.TabIndex = 0;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.PictureBox11_Click);
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.BackColor = System.Drawing.Color.DarkOrange;
+            this.pictureBox12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox12.Location = new System.Drawing.Point(249, 114);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(104, 96);
+            this.pictureBox12.TabIndex = 1;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.PictureBox12_Click);
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.BackColor = System.Drawing.Color.DarkOrange;
+            this.pictureBox13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox13.Location = new System.Drawing.Point(359, 114);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(106, 96);
+            this.pictureBox13.TabIndex = 2;
+            this.pictureBox13.TabStop = false;
+            this.pictureBox13.Click += new System.EventHandler(this.PictureBox13_Click);
+            // 
+            // pictureBox23
+            // 
+            this.pictureBox23.BackColor = System.Drawing.Color.DarkOrange;
+            this.pictureBox23.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox23.Location = new System.Drawing.Point(359, 216);
+            this.pictureBox23.Name = "pictureBox23";
+            this.pictureBox23.Size = new System.Drawing.Size(106, 96);
+            this.pictureBox23.TabIndex = 5;
+            this.pictureBox23.TabStop = false;
+            this.pictureBox23.Click += new System.EventHandler(this.PictureBox23_Click);
+            // 
+            // pictureBox22
+            // 
+            this.pictureBox22.BackColor = System.Drawing.Color.DarkOrange;
+            this.pictureBox22.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox22.Location = new System.Drawing.Point(249, 216);
+            this.pictureBox22.Name = "pictureBox22";
+            this.pictureBox22.Size = new System.Drawing.Size(104, 96);
+            this.pictureBox22.TabIndex = 4;
+            this.pictureBox22.TabStop = false;
+            this.pictureBox22.Click += new System.EventHandler(this.PictureBox22_Click);
+            // 
+            // pictureBox21
+            // 
+            this.pictureBox21.BackColor = System.Drawing.Color.DarkOrange;
+            this.pictureBox21.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox21.Location = new System.Drawing.Point(137, 216);
+            this.pictureBox21.Name = "pictureBox21";
+            this.pictureBox21.Size = new System.Drawing.Size(106, 96);
+            this.pictureBox21.TabIndex = 3;
+            this.pictureBox21.TabStop = false;
+            this.pictureBox21.Click += new System.EventHandler(this.PictureBox21_Click);
+            // 
+            // pictureBox33
+            // 
+            this.pictureBox33.BackColor = System.Drawing.Color.DarkOrange;
+            this.pictureBox33.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox33.Location = new System.Drawing.Point(359, 318);
+            this.pictureBox33.Name = "pictureBox33";
+            this.pictureBox33.Size = new System.Drawing.Size(106, 96);
+            this.pictureBox33.TabIndex = 8;
+            this.pictureBox33.TabStop = false;
+            this.pictureBox33.Click += new System.EventHandler(this.PictureBox33_Click);
+            // 
+            // pictureBox32
+            // 
+            this.pictureBox32.BackColor = System.Drawing.Color.DarkOrange;
+            this.pictureBox32.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox32.Location = new System.Drawing.Point(249, 318);
+            this.pictureBox32.Name = "pictureBox32";
+            this.pictureBox32.Size = new System.Drawing.Size(104, 96);
+            this.pictureBox32.TabIndex = 7;
+            this.pictureBox32.TabStop = false;
+            this.pictureBox32.Click += new System.EventHandler(this.PictureBox32_Click);
+            // 
+            // pictureBox31
+            // 
+            this.pictureBox31.BackColor = System.Drawing.Color.DarkOrange;
+            this.pictureBox31.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox31.Location = new System.Drawing.Point(137, 318);
+            this.pictureBox31.Name = "pictureBox31";
+            this.pictureBox31.Size = new System.Drawing.Size(106, 96);
+            this.pictureBox31.TabIndex = 6;
+            this.pictureBox31.TabStop = false;
+            this.pictureBox31.Click += new System.EventHandler(this.PictureBox31_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.DarkOrange;
+            this.resetButton.Location = new System.Drawing.Point(250, 58);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(103, 35);
+            this.resetButton.TabIndex = 9;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 11;
+            // 
+            // nolbox
+            // 
+            this.nolbox.Location = new System.Drawing.Point(135, 58);
+            this.nolbox.Name = "nolbox";
+            this.nolbox.ReadOnly = true;
+            this.nolbox.Size = new System.Drawing.Size(100, 22);
+            this.nolbox.TabIndex = 12;
+            // 
+            // krestbox
+            // 
+            this.krestbox.Location = new System.Drawing.Point(365, 58);
+            this.krestbox.Name = "krestbox";
+            this.krestbox.ReadOnly = true;
+            this.krestbox.Size = new System.Drawing.Size(100, 22);
+            this.krestbox.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(362, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Победы крестиков";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(119, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Победы ноликов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(687, 647);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.krestbox);
+            this.Controls.Add(this.nolbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.pictureBox33);
+            this.Controls.Add(this.pictureBox32);
+            this.Controls.Add(this.pictureBox31);
+            this.Controls.Add(this.pictureBox23);
+            this.Controls.Add(this.pictureBox22);
+            this.Controls.Add(this.pictureBox21);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Name = "Form1";
+            this.Text = "Крестики-нолики";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox23)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox33)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox32)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox31)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox23;
+        private System.Windows.Forms.PictureBox pictureBox22;
+        private System.Windows.Forms.PictureBox pictureBox21;
+        private System.Windows.Forms.PictureBox pictureBox33;
+        private System.Windows.Forms.PictureBox pictureBox32;
+        private System.Windows.Forms.PictureBox pictureBox31;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nolbox;
+        private System.Windows.Forms.TextBox krestbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ KrestikNolik/KrestikNolik/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KrestikNolik
+{
+    public partial class Form1 : Form
+    { 
+        int krestwins;
+        int nolwins;
+        private PictureBox[,] pbx = new PictureBox[4, 4];
+        private Image Krestik;
+        private Image Nolik;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Krestik = Image.FromFile("C:\\Users\\EM\\Downloads\\krestik.jpg");
+            Nolik = Image.FromFile("C:\\Users\\EM\\Downloads\\Nolik.jpg");
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 1; j < 4; j++)
+                {
+                    array[i, j] = 5;
+                }
+            };
+        }
+
+        int[,] array = new int[4, 4];
+
+        int counter = 0;
+
+        private void PictureBox11_Click(object sender, EventArgs e)
+        {
+            if (array[1, 1] == 5)
+            {
+                if (counter % 2 == 0)
+                {
+                    pictureBox11.Image = Krestik;
+                    pictureBox11.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[1, 1] = 1;
+                }
+                else
+                {
+                    pictureBox11.Image = Nolik;
+                    pictureBox11.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[1, 1] = 0;
+                }
+                counter++;
+                CheckWinner();
+            }
+        }
+
+        private void PictureBox12_Click(object sender, EventArgs e)
+        {
+            if (array[1, 2] == 5)
+            {
+                if (counter % 2 == 0)
+                {
+                    pictureBox12.Image = Krestik;
+                    pictureBox12.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[1, 2] = 1;
+                }
+                else
+                {
+                    pictureBox12.Image = Nolik;
+                    pictureBox12.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[1, 2] = 0;
+                }
+                counter++;
+                CheckWinner();
+            }
+        }
+
+        private void PictureBox13_Click(object sender, EventArgs e)
+        {
+            if (array[1, 3] == 5)
+            {
+                if (counter % 2 == 0)
+                {
+                    pictureBox13.Image = Krestik;
+                    pictureBox13.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[1, 3] = 1;
+                }
+                else
+                {
+                    pictureBox13.Image = Nolik;
+                    pictureBox13.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[1, 3] = 0;
+                }
+                counter++;
+                CheckWinner();
+            }
+        }
+
+        private void PictureBox21_Click(object sender, EventArgs e)
+        {
+            if (array[2, 1] == 5)
+            {
+                if (counter % 2 == 0)
+                {
+                    pictureBox21.Image = Krestik;
+                    pictureBox21.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[2, 1] = 1;
+                }
+                else
+                {
+                    pictureBox21.Image = Nolik;
+                    pictureBox21.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[2, 1] = 0;
+                }
+                counter++;
+                CheckWinner();
+            }
+        }
+
+        private void PictureBox22_Click(object sender, EventArgs e)
+        {
+            if (array[2, 2] == 5)
+            {
+                if (counter % 2 == 0)
+                {
+                    pictureBox22.Image = Krestik;
+                    pictureBox22.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[2, 2] = 1;
+                }
+                else
+                {
+                    pictureBox22.Image = Nolik;
+                    pictureBox22.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[2, 2] = 0;
+                }
+                counter++;
+                CheckWinner();
+            }
+        }
+
+        private void PictureBox23_Click(object sender, EventArgs e)
+        {
+            if (array[2, 3] == 5)
+            {
+                if (counter % 2 == 0)
+                {
+                    pictureBox23.Image = Krestik;
+                    pictureBox23.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[2, 3] = 1;
+                }
+                else
+                {
+                    pictureBox23.Image = Nolik;
+                    pictureBox23.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[2, 3] = 0;
+                }
+                counter++;
+                CheckWinner();
+            }
+        }
+
+        private void PictureBox31_Click(object sender, EventArgs e)
+        {
+            if (array[3, 1] == 5)
+            {
+                if (counter % 2 == 0)
+                {
+                    pictureBox31.Image = Krestik;
+                    pictureBox31.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[3, 1] = 1;
+                }
+                else
+                {
+                    pictureBox31.Image = Nolik;
+                    pictureBox31.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[3, 1] = 0;
+                }
+                counter++;
+                CheckWinner();
+            }
+        }
+
+        private void PictureBox32_Click(object sender, EventArgs e)
+        {
+            if (array[3, 2] == 5)
+            {
+                if (counter % 2 == 0)
+                {
+                    pictureBox32.Image = Krestik;
+                    pictureBox32.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[3, 2] = 1;
+                }
+                else
+                {
+                    pictureBox32.Image = Nolik;
+                    pictureBox32.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[3, 2] = 0;
+                }
+                counter++;
+                CheckWinner();
+            }
+        }
+        private void PictureBox33_Click(object sender, EventArgs e)
+        {
+            if (array[3, 3] == 5)
+            {
+                if (counter % 2 == 0)
+                {
+                    pictureBox33.Image = Krestik;
+                    pictureBox33.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[3, 3] = 1;
+                }
+                else
+                {
+                    pictureBox33.Image = Nolik;
+                    pictureBox33.SizeMode = PictureBoxSizeMode.CenterImage;
+                    array[3, 3] = 0;
+                }
+                counter++;
+                CheckWinner();
+            }
+        }
+        
+        int winner = 10;
+        private void CheckWinner()
+        {
+            //победа крестиков
+            for (int i = 1; i < 4; i++)
+            {
+                if (array[i, 1] == 1 && array[i, 2] == 1 && array[i, 3] == 1)
+                {
+                    MessageBox.Show("Победа крестиков");
+                    krestwins++;
+                    krestbox.Text = "" + krestwins + "";
+                    reset();
+
+                }
+                if (array[1, i] == 1 && array[2, i] == 1 && array[3, i] == 1)
+                {
+                    MessageBox.Show("Победа крестиков");
+                    krestwins++;
+                    krestbox.Text = "" + krestwins + "";
+                    reset();
+                }
+            }
+
+            if (array[1, 1] == 1 && array[2, 2] == 1 && array[3, 3] == 1)
+            {
+                MessageBox.Show("Победа крестиков");
+                krestwins++;
+                krestbox.Text = "" + krestwins + "";
+                reset();
+            }
+
+            if (array[1, 3] == 1 && array[2, 2] == 1 && array[3, 1] == 1)
+            {
+                MessageBox.Show("Победа крестиков");
+                krestwins++;
+                krestbox.Text = "" + krestwins + "";
+                reset();
+            }
+            //победа ноликов
+            for (int i = 1; i < 4; i++)
+            {
+                if (array[i, 1] == 0 && array[i, 2] == 0 && array[i, 3] == 0)
+                {
+                    MessageBox.Show("Победа ноликов");
+                    nolwins++;
+                    nolbox.Text = "" + nolwins + "";
+                    reset();
+                }
+                if (array[1, i] == 0 && array[2, i] == 0 && array[3, i] == 0)
+                {
+                    MessageBox.Show("Победа ноликов");
+                    nolwins++;
+                    nolbox.Text = "" + nolwins + "";
+                    reset();
+                }
+            }
+
+            if (array[1, 1] == 0 && array[2, 2] == 0 && array[3, 3] == 0)
+            {
+                MessageBox.Show("Победа ноликов");
+                nolwins++;
+                nolbox.Text = "" + nolwins + "";
+                reset();
+            }
+
+            if (array[1, 3] == 0 && array[2, 2] == 0 && array[3, 1] == 0)
+            {
+                MessageBox.Show("Победа ноликов");
+                nolwins++;
+                nolbox.Text = "" + nolwins + "";
+                reset();
+            }
+            //ничья
+            if (counter == 9 && winner == 10)
+            {
+                winner = 5;
+                MessageBox.Show("Ничья");
+                reset();
+            }
+        }
+        private void reset_Click(object sender, EventArgs e) { reset(); }
+        private void reset()
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 1; j < 4; j++)
+                {
+                    array[i, j] = 5;
+                }
+            }
+            counter = 0;
+            winner = 10;
+            pictureBox11.Image = null;
+            pictureBox12.Image = null;
+            pictureBox13.Image = null;
+            pictureBox21.Image = null;
+            pictureBox22.Image = null;
+            pictureBox23.Image = null;
+            pictureBox31.Image = null;
+            pictureBox32.Image = null;
+            pictureBox33.Image = null;
+        }
+    }
+}
+
